c0ea50904eeabf7820d8e68b69952ef79250bed7
Rupesh Lakshman <codewithruppu@gmail.com>
2025-09-25 21:32:15 +0530

    add css,sass,postcss,hotreloading,browserslist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles/index.scss";
+
 const person = {
 	walk: () => `yes`,
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 let mode = "development";
+let target = "web";
 if (process.env.NODE_ENV === "production") {
 	mode = "production";
+	target = "browserslist";
 }
 
 module.exports = {
 	mode,
+	target,
 	/* devtool: false, 'source-map' */
 	devtool: "source-map",
 
@@ -17,12 +23,28 @@ module.exports = {
 					loader: "babel-loader",
 				},
 			},
+			{
+				test: /\.(s[ac]|c)ss$/i,
+				/*
+				   test: /\.s?css$/i,
+            */
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"postcss-loader",
+					"sass-loader",
+				],
+			},
 		],
 	},
 
+	plugins: [new MiniCssExtractPlugin()],
+
 	devServer: {
 		static: {
-			directory: "./dist",
+			directory: path.join(__dirname, "dist"),
 		},
+		port: 3000,
+		hot: true,
 	},
 };
