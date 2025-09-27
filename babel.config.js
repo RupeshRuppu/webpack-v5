21f903436b4b255b26cc6babc89596bdf6c5af4d
Rupesh Lakshman <codewithruppu@gmail.com>
2025-09-27 23:32:07 +0530

    fixed build with react-refresh

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
+const isDevelopment = process.env.NODE_ENV === "development";
+
 module.exports = {
 	presets: [
 		"@babel/preset-env",
@@ -8,7 +10,7 @@ module.exports = {
 			},
 		],
 	],
-	plugins: ["react-refresh/babel"],
+	plugins: [isDevelopment && "react-refresh/babel"].filter(Boolean),
 };
 
 /*
